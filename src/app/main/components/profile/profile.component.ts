638fba51df7040613aafc51f9e6c46993218ffdf
brilux <brilux96@gmail.com>
2019-04-03 15:46:09 +0300

    add nav icons, moved logout to profile and fix feed posts

--- src/app/main/components/profile/profile.component.ts
+++ src/app/main/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../../rest/profile/profile.service';
+import { AuthService } from '../../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +12,17 @@ export class ProfileComponent implements OnInit {
 
   userInfo = {};
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.profileService.userInfo().subscribe(info => this.userInfo = info.user);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
   }
 
 }
