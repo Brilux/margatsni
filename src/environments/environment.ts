289dc0df154ad19682f509fd63335740c38e301a
brilux <brilux96@gmail.com>
2019-05-11 21:29:41 +0300

    remove edit-post and share service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'https://margatsnimoc.herokuapp.com/api/v1'
+  api: 'https://api.margatsni.ml/api/v1'
+  // api: 'https://margatsnimoc.herokuapp.com/api/v1'
 };
 /*
  * For easier debugging in development mode, you can import the following file
